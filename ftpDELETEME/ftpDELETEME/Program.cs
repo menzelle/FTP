de5b504b1d7622c9068a792f0f842567c09c9a4f
menzelle <35540661+menzelle@users.noreply.github.com>
2020-03-31 23:11:23 -0700

    Add files via upload

---
+++ ftpDELETEME/ftpDELETEME/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace ftpDELETEME
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Create Object Instance */
+            ftp ftpClient = new ftp(@"ftp://ftp.aeroxchange.com/", "aerocontrols_admin", "@er0cnt_ftp");
+
+            /* Upload a File */
+            ftpClient.upload("test.xlsx", @"\\aci-cluster\D$\VOL1\SHARE\SALES\GENERAL\REPORTS\AeroXChangeUploads\AeroRepairUploadFromProcessor.xlsx");
+
+
+            /* Release Resources */
+            ftpClient = null;
+        }
+    }
+}
---
+++ ftpDELETEME/ftpDELETEME/ftp.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ftpDELETEME
+{
+    class ftp
+    {
+        private string host = @"ftp://ftp.aeroxchange.com/";
+        private string user = "aerocontrols_admin";
+        private string pass = "@er0cnt_ftp";
+        private WebRequest ftpRequest = null;
+        private WebResponse ftpResponse = null;
+        private Stream ftpStream = null;
+        private int bufferSize = 2048;
+
+        /* Construct Object */
+        public ftp(string hostIP, string userName, string password) { host = hostIP; user = userName; pass = password; }
+
+        /* Download File */
+      
+        /* Upload File */
+        public void upload(string remoteFile, string localFile)
+        {
+            try
+            {
+                /* Create an FTP Request */
+                ftpRequest = WebRequest.Create(@"ftp://aeroxchange.com/");
+                /* Log in to the FTP Server with the User Name and Password Provided */
+                ftpRequest.Credentials = new NetworkCredential(user, pass);
+           
+                /* Specify the Type of FTP Request */
+                ftpRequest.Method = WebRequestMethods.Ftp.UploadFile;
+                /* Establish Return Communication with the FTP Server */
+                ftpStream = ftpRequest.GetRequestStream();
+                /* Open a File Stream to Read the File for Upload */
+                FileStream localFileStream = new FileStream(localFile, FileMode.Create);
+                /* Buffer for the Downloaded Data */
+                byte[] byteBuffer = new byte[bufferSize];
+                int bytesSent = localFileStream.Read(byteBuffer, 0, bufferSize);
+                /* Upload the File by Sending the Buffered Data Until the Transfer is Complete */
+                try
+                {
+                    while (bytesSent != 0)
+                    {
+                        ftpStream.Write(byteBuffer, 0, bytesSent);
+                        bytesSent = localFileStream.Read(byteBuffer, 0, bufferSize);
+                    }
+                }
+                catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+                /* Resource Cleanup */
+                localFileStream.Close();
+                ftpStream.Close();
+                ftpRequest = null;
+            }
+            catch (Exception ex) { Console.WriteLine(ex.ToString()); }
+            return;
+        }
+
+  
+    }
+}
